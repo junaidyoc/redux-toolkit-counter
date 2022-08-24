d50e3a926565f3fdffdae32bf99d7e1e6b31631a
junaidsheikh45 <85952271+junaidsheikh45@users.noreply.github.com>
2022-08-24 20:46:55 +0500

    'redux-tool-kit-counter-app'

---
+++ src/redux/counter/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: 0,
+}
+
+// Creating a slice
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.value +=1
+        },
+        decrement: (state) => {
+            state.value --
+        },
+    },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
